ca2f8e84fe4106fe68cdd29f8313cf0e7cf1b870
Adtiya <30702833+Adtiya@users.noreply.github.com>
2022-05-26 11:22:31 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from "react";
+import "./App.css";
 
-import Quiz from './Quiz.js'
+import Quiz from "./Quiz.js";
 
 class App extends Component {
   render() {
-    return (
-      <div />
-      <Quiz />
-    )
+    return <Quiz />;
   }
 }
+
+export default App;
--- src/Quiz.js
+++ src/Quiz.js
@@ -17,3 +17,5 @@ class Quiz extends Component {
     );
   }
 }
+
+export default Quiz;
